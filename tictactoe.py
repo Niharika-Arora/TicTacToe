b8a8f8e0005ad4328e715714d560c3f12b61d89f
Niharika-Arora <aroraniharika15@gmail.com>
2019-12-23 19:20:00 +0530

    Menu almost done

--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,6 @@
+import sys
+
+
 def tac_board(board):
     print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
     print('-----------')
@@ -6,11 +9,28 @@ def tac_board(board):
     print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
 
 
+def menu():
+    ch = input("Do you want to play? (yes or no) : ")
+    if ch.casefold() == "no":
+        sys.exit(0)
+    elif ch.casefold() == "yes":
+        print("Who goes first?")
+        choice = input("Enter 'p' for player and 'c' or computer : ")
+        return choice
+
+
 def check_win(brd, let):
-    return (brd[1] == let and brd[2] == let and brd[3] == let) or (brd[4] == let and brd[5] == let and brd[6] == let) or (brd[7] == let and brd[8] == let and brd[9] == let) or (brd[1] == let and brd[4] == let and brd[7] == let) or (brd[2] == let and brd[5] == let and brd[8] == let) or (brd[3] == let and brd[6] == let and brd[9] == let) or (brd[1] == let and brd[5] == let and brd[9] == let) or (brd[3] == let and brd[5] == let and brd[7] == let)
+    return (brd[1] == let and brd[2] == let and brd[3] == let) or (
+            brd[4] == let and brd[5] == let and brd[6] == let) or (
+            brd[7] == let and brd[8] == let and brd[9] == let) or (
+            brd[1] == let and brd[4] == let and brd[7] == let) or (
+            brd[2] == let and brd[5] == let and brd[8] == let) or (
+            brd[3] == let and brd[6] == let and brd[9] == let) or (
+            brd[1] == let and brd[5] == let and brd[9] == let) or (
+            brd[3] == let and brd[5] == let and brd[7] == let)
 
 
 if __name__ == "__main__":
-    print("******Welcome******")
+    print("******Welcome to the game of Tic Tac Toe******")
     while True:
-        board = [' ' for i in range(10)]
+        bord = [' ' for i in range(10)]
